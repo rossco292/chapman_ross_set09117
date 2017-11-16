1eed5e10190d98003b26880b4d56a4cc3a3e925d
rossco292 <rosscochapman2@gmail.com>
2017-11-16 18:10:27 +0000

    Add files via upload
    Finished commented checkers program and finished report

--- Checkers.py
+++ Checkers.py
@@ -1,4 +1,5 @@
-from enum import Enum #This is so that i can have an enum for selecting whos turn it is
+from enum import Enum
+#This is so that i can have an enum for selecting whos turn it is
 #These are constants so that it is clear what i am using in my code
 SYMBOL_KINGBLACK = 'B'
 SYMBOL_KINGWHITE = 'W'
@@ -21,6 +22,7 @@ def main():# This is the main where everything is essentially run
         white_king_total = sum(z.count("W") for z in Pieces)
         white_total = white_king_total + white_pawn_total# In these 2 values the players kings and pawns are added together to get the total pieces each player has
         black_total = black_king_total + black_pawn_total
+        
         if white_total == 0:#This if deals with the win condition
             print("Black player has won the game")
             qt = str(input("Do you want to quit the game y/n"))#This smaller if essentily asks if you want to quit the game so you can see who won
@@ -63,12 +65,12 @@ def move(Turns, white_total, black_total, Pieces):
     midval_y = abs(src_y + dst_y) // 2
     if Turns["Turn"] == players.Black:#This checks who's turn it is
         
-        if Pieces[src_y][src_x] == SYMBOL_WHITE or Pieces[src_y][src_x] == SYMBOL_KINGWHITE:
+        if Pieces[src_y][src_x] == SYMBOL_WHITE or Pieces[src_y][src_x] == SYMBOL_KINGWHITE:#This checks that the correct player has selected their pieces and not the opponents
             print("Wrong piece selected try again ")
             print_board(Pieces)
-            return move(Turns, white_total, black_total, Pieces)
-#Movement logic
-        if Pieces[src_y][src_x] == SYMBOL_BLACK and src_y > dst_y:
+            return move(Turns, white_total, black_total, Pieces)#This returns back to the move and lets the player take their turn
+
+        if Pieces[src_y][src_x] == SYMBOL_BLACK and src_y > dst_y:#These check how a piece can move
             print("Pawn cannot move backwards")
             return move(Turns, white_total, black_total, Pieces)
         if src_x == dst_x:
@@ -92,54 +94,55 @@ def move(Turns, white_total, black_total, Pieces):
             
         
             
-        if dst_y == 7:
+        if dst_y == 7:#This checks if a king piece has been made
             print("Congrats you have made a king Piece")
-            Pieces[src_y][src_x] = SYMBOL_EMPTY
-            Pieces[7][dst_x] = SYMBOL_KINGBLACK
+            Pieces[src_y][src_x] = SYMBOL_EMPTY#This makes the source coords to empty so that an extra piece is not left
+            Pieces[7][dst_x] = SYMBOL_KINGBLACK#This changes the destination to a king piece
             return
             
-        if Pieces[dst_y][dst_x] == SYMBOL_BLACK or Pieces[dst_y][dst_x] == SYMBOL_KINGBLACK:
+        if Pieces[dst_y][dst_x] == SYMBOL_BLACK or Pieces[dst_y][dst_x] == SYMBOL_KINGBLACK:#This checks if the destination is already occupied
             print("cannot move there as there is already one of your pieces there")
             return move(Turns, white_total, black_total, Pieces)
         
-        if Pieces[dst_y][dst_x] == SYMBOL_WHITE or Pieces[dst_y][dst_x] == SYMBOL_KINGWHITE:
+        if Pieces[dst_y][dst_x] == SYMBOL_WHITE or Pieces[dst_y][dst_x] == SYMBOL_KINGWHITE:#This checks if the destination is occupied by an opponents piece
             print("cannot move there as there is already an opponents piece there")
             return move(Turns, white_total, black_total, Pieces)
         
-        if Pieces[midval_y][midval_x] == SYMBOL_WHITE or Pieces[midval_y][midval_x] == SYMBOL_KINGWHITE:
+        if Pieces[midval_y][midval_x] == SYMBOL_WHITE or Pieces[midval_y][midval_x] == SYMBOL_KINGWHITE:#This is for taking both pawns and kings
             Pieces[midval_y][midval_x] = SYMBOL_EMPTY
             print_board(Pieces)
-            return white_total
+            return 
 
        
         
-        if Pieces[src_y][src_x] == SYMBOL_BLACK:
+        if Pieces[src_y][src_x] == SYMBOL_BLACK:#This is for moveing pieces to an empty space
             Pieces[dst_y][dst_x] = SYMBOL_BLACK
             Pieces[src_y][src_x] = SYMBOL_EMPTY
             print_board(Pieces)
             
-        if Pieces[src_y][src_x] == SYMBOL_KINGBLACK:
+        if Pieces[src_y][src_x] == SYMBOL_KINGBLACK:#This is for moveing kings to an empty space
             Pieces[dst_y][dst_x] = SYMBOL_KINGBLACK
             Pieces[src_y][src_x] = SYMBOL_EMPTY
             print_board(Pieces)
-        Decision = str(input("Continue y/n?"))
-        if Decision == "n":
+        Decision = str(input("Continue y/n?"))#This asks if you want to continue playing the game
+        if Decision == "n":#This checks if the user wants to quit then quits
             exit()
         
-        Turns["Turn"] = players.White
+        Turns["Turn"] = players.White#This sets the player to white
         return 
         
-    if Turns["Turn"] == players.White:
+    if Turns["Turn"] == players.White:#This checks if the player is the white one
         
-        if Pieces[src_y][src_x] == SYMBOL_BLACK:
+        if Pieces[src_y][src_x] == SYMBOL_BLACK:#This checks if you have selected the correct piece
             print("Wrong piece selected try again")
             print_board(Pieces)
             return move(Turns, white_total, black_total, Pieces)
-        if Pieces[src_y][src_x] == SYMBOL_WHITE and src_y < dst_y:
+        
+        if Pieces[src_y][src_x] == SYMBOL_WHITE and src_y < dst_y:#These check the movement/how a piece can move
             print("Pawn cannot move backwards")
             print_board(Pieces)
             return move(Turns, white_total, black_total, Pieces)
-#Movement logic       
+       
         if src_x == dst_x:
             print("You cant move a piece verticaly")
             return move(Turns, white_total, black_total, Pieces)
@@ -159,41 +162,41 @@ def move(Turns, white_total, black_total, Pieces):
             print("You cant move farther than 2 spaces in a jump")
             return move(Turns, white_total, black_total, Pieces)
 
-        if dst_y == 0:
+        if dst_y == 0:#This checks if a king piece has been made
             print("Congrats you have made a king Piece")
             Pieces[src_y][src_x] = SYMBOL_EMPTY
             Pieces[0][dst_x] = SYMBOL_KINGWHITE
             print_board(Pieces)
             return
 
-        if Pieces[dst_y][dst_x] == SYMBOL_BLACK or Pieces[dst_y][dst_x] == SYMBOL_KINGBLACK:
+        if Pieces[dst_y][dst_x] == SYMBOL_BLACK or Pieces[dst_y][dst_x] == SYMBOL_KINGBLACK:#This checks if the destination is empty for a piece to move to
             print("cannot move there as there is already one of your opponets pieces there")
             return move(Turns, white_total, black_total, Pieces)
         
-        if Pieces[dst_y][dst_x] == SYMBOL_WHITE or Pieces[dst_y][dst_x] == SYMBOL_KINGWHITE:
+        if Pieces[dst_y][dst_x] == SYMBOL_WHITE or Pieces[dst_y][dst_x] == SYMBOL_KINGWHITE:#This checks if the destination is empty for a piece to move to
             print("cannot move there as there is already one of your pieces there")
             return move(Turns, white_total, black_total, Pieces)
         
-        if Pieces[midval_y][midval_x] == SYMBOL_BLACK or Pieces[midval_y][midval_x] == SYMBOL_KINGBLACK:
+        if Pieces[midval_y][midval_x] == SYMBOL_BLACK or Pieces[midval_y][midval_x] == SYMBOL_KINGBLACK:#This checks if the correct pieces are being taken
             Pieces[midval_y][midval_x] = SYMBOL_EMPTY
             print_board(Pieces)
            
             
-        if Pieces[src_y][src_x] == SYMBOL_WHITE:
+        if Pieces[src_y][src_x] == SYMBOL_WHITE:#This moves a pawn piece
             Pieces[dst_y][dst_x] = SYMBOL_WHITE
             Pieces[src_y][src_x] = SYMBOL_EMPTY
             print_board(Pieces)
             
         
-        if Pieces[src_y][src_x] == SYMBOL_KINGWHITE:
+        if Pieces[src_y][src_x] == SYMBOL_KINGWHITE:#This moves a king Piece
             Pieces[dst_y][dst_x] = SYMBOL_KINGWHITE
             Pieces[src_y][src_x] = SYMBOL_EMPTY
             print_board(Pieces)
-        Decision = str(input("Continue y/n?"))
-        if Decision == "n":
+        Decision = str(input("Continue y/n?"))#This asks if you want to continue playing the game
+        if Decision == "n":#This exits the program
             exit()
         
-        Turns["Turn"] = players.Black
+        Turns["Turn"] = players.Black#This changes the player to black
         return
         
     
